32583d8181bdbbbe14322ca6eff8504543ae7ae6
erabdullah2k21 <ali.adnan28@gmail.com>
2021-09-04 11:34:49 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
+
 function App() {
+    const[value,updateValue]= useState(true)
+    const button=()=>{
+        updateValue(!value)
+    }
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+   {value?(
+     <div>
+     <p>Please Sign up.</p>
+     <button className="button" onClick={button}>
+       LOGIN
+     </button>
+     </div>
+   ):(
+     <div>
+       <p>Welcome back!</p>
+       <button className="button" onClick={button}>LOGOUT</button>
+       </div>
+   )
+   
+   }
+  
+   </div>
   );
 }
 
